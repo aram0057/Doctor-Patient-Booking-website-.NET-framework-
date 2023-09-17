80d8c9500fa353441c6bb3ffe05ae8f458248682
aram0057 <aram0057@student.monash.edu>
2023-09-17 17:59:35 +1000

    Portfolio with doctor and patient tables created with basic setup.

---
+++ FIT5032 Portfolio/33526559_Abbishek_Portfolio/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="_33526559_Abbishek_Portfolio.MvcApplication" Language="C#" %>
---
+++ FIT5032 Portfolio/33526559_Abbishek_Portfolio/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(_33526559_Abbishek_Portfolio.Startup))]
+namespace _33526559_Abbishek_Portfolio
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
